9e63ff9a68ce5cbd4315f59d1777cf5f3825ffbd
l00pinfinity <collinskipkemboi001@gmail.com>
2023-04-24 12:17:23 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription, take } from 'rxjs';
+import { User } from './core/models/user';
+import { UserService } from './core/services/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'observable-async';
+export class AppComponent implements OnInit, OnDestroy {
+
+  subscription!: Subscription;
+  userData$: Observable<User[]> = this.userService.getUsers().pipe(take(1));
+  users: User[] = [];
+
+  constructor(private userService: UserService){}
+
+  ngOnInit(): void {
+    this.subscription = this.userData$.subscribe((users:User[]) =>{
+      this.users = users;
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
---
+++ src/app/core/services/user.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../models/user';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private httpClient:HttpClient) { }
+
+  getUsers(): Observable<User[]>{
+    return this.httpClient.get<User[]>(environment.BASE_URL + '/users');
+  }
+}
