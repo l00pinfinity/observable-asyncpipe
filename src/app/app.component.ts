ab8918a1392e195157479f77c73a76ee7f060480
l00pinfinity <collinskipkemboi001@gmail.com>
2023-04-24 12:23:07 +0300

    feat: handle change detection due to manual subscriptions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import { Observable, Subscription, take } from 'rxjs';
 import { User } from './core/models/user';
 import { UserService } from './core/services/user.service';
@@ -6,7 +6,8 @@ import { UserService } from './core/services/user.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent implements OnInit, OnDestroy {
 
@@ -14,11 +15,12 @@ export class AppComponent implements OnInit, OnDestroy {
   userData$: Observable<User[]> = this.userService.getUsers().pipe(take(1));
   users: User[] = [];
 
-  constructor(private userService: UserService){}
+  constructor(private userService: UserService,private changeDetectorRef:ChangeDetectorRef){}
 
   ngOnInit(): void {
     this.subscription = this.userData$.subscribe((users:User[]) =>{
       this.users = users;
+      this.changeDetectorRef.markForCheck(); // When we have a change, we explicitly markForCheck();
     })
   }
 
